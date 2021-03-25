86d61f930a5700b3f146524e16d6627f7b6ef89d
fnnaeem1881 <77448606+fnnaeem1881@users.noreply.github.com>
2021-03-25 11:39:23 +0600

    index.php
    Test Project

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+  <head>
+    <title>reCAPTCHA demo: Simple page</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  </head>
+  <body>
+    <form action="?" method="POST">
+      <div class="g-recaptcha" data-sitekey="6Lcu2o0aAAAAANW23mSDRVGpsG7DBXt_gVomtGRa"></div>
+      <br/>
+      <input type="submit" value="Submit">
+    </form>
+    
+
+</script>
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
+  </body>
+</html>
